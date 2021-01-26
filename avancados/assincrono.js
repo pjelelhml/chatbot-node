2dcd0184fde6f419ee6ca39f1e9f456678761284
pjelelhml <paulo3385@hotmail.com>
2021-01-25 20:30:11 -0400

    programa que torna possível enviar msg apartir de url com o bot, sem precisar do usuário

---
+++ avancados/assincrono.js
@@ -0,0 +1,24 @@
+const env = require('../.env')
+const Telegram = require('telegraf/telegram')
+const axios = require('axios')
+const Markup = require('telegraf/markup')
+
+const enviarMensagem = msg => {
+    axios.get(`${env.apiUrl}/sendMessage?chat_id=${env.userID}&text=${encodeURI(msg)}`)
+        .catch(e => console.log(e))
+}
+
+// setInterval(() => {
+//     enviarMensagem('Enviando mensagem de forma assíncrona.')
+// }, 3000)
+
+enviarMensagem('Enviando mensagem de forma assíncrona.')
+
+
+const teclado = Markup.keyboard([
+    ['OK', 'Cancelar']
+]).resize().oneTime().extra()
+
+const telegram = new Telegram(env.token)
+
+telegram.sendMessage(env.userID, 'Essa é uma mensagem com teclado', teclado)
